c5265d61bbfd7eea02f422ffd56f24b5206fe045
faridasabry <faridasabry@gmail.com>
2022-09-13 13:20:32 +0300

    Update data_loading.py
    --adding fasting data option

--- data_loading.py
+++ data_loading.py
@@ -87,13 +87,15 @@ def real_data_loading (data_name, seq_len):
   Returns:
     - data: preprocessed data.
   """  
-  assert data_name in ['stock','energy']
+  assert data_name in ['stock','energy','fasting']
   
   if data_name == 'stock':
     ori_data = np.loadtxt('data/stock_data.csv', delimiter = ",",skiprows = 1)
   elif data_name == 'energy':
     ori_data = np.loadtxt('data/energy_data.csv', delimiter = ",",skiprows = 1)
-        
+  elif data_name == 'fasting':
+    ori_data = np.loadtxt('data/fasting_data.csv', delimiter = ",",skiprows = 1)
+    
   # Flip the data to make chronological data
   ori_data = ori_data[::-1]
   # Normalize the data
