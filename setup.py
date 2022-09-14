6a57c07f9afc74e2bd09880403c47d049fa9363a
faridasabry <faridasabry@gmail.com>
2022-09-14 09:24:17 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read_requirements():
+    """Parse requirements from requirements.txt."""
+    reqs_path = os.path.join('.', 'requirements.txt')
+    with open(reqs_path, 'r') as f:
+        requirements = [line.rstrip() for line in f]
+    return requirements
+
+
+console_scripts = [
+]
+
+setup(
+    name='attacksplitnn',
+    version='0.0.0',
+    description='TimeGAN for wearable data',
+    author='faridasabry',
+    author_email='a@b.com',
+    license="MIT",
+    install_requires=read_requirements(),
+    url="https://github.com/faridasabry/TimeGAN-forWearableData",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    entry_points={'console_scripts': console_scripts},
+)
